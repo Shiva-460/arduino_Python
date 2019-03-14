251c697b354dd14b1cbdab9afdfa9b62cdf80a7d
alex5386 <alexngai5386@gmail.com>
2019-03-13 21:57:19 -0700

    Add files via upload

---
+++ pickup_function.ino
@@ -0,0 +1,26 @@
+unsigned long start=0
+unsigned long current=0;
+
+
+pickup(int x,int z){
+  int x1=0;
+  int z1=0;
+  Drive(x,z)
+  current=millis()
+  if(Serial.read()==false&& ((current-start)==1000){
+
+    return drive(x1,z1);
+    
+  }else if (Serial.read()==false){
+
+
+
+    start=millis();
+  }
+
+
+  
+
+
+  
+}
